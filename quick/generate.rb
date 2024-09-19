4350faa88b885f5d68d935e5ca625012bb88f88c
Gerald Bauer <gerald.bauer@gmail.com>
2024-09-19 19:25:16 +0200

    up

--- quick/generate.rb
+++ quick/generate.rb
@@ -7,46 +7,53 @@ require 'sportdb/quick'
 
 # '2024-25',
 datasets = {
-    'en.1'  =>  ['england',            '1-premierleague', 'Premier League'],
+    'en.1'  =>  ['england',            '1-premierleague',    'Premier League'],
+    'en.2'  =>  ['england',            '2-championship',    'Championship'],
     'es.1'  =>  ['espana',             '1-liga',          'Primera División'],
-    'de.1'  =>  ['deutschland',        '1-bundesliga',    'Bundesliga'],
+    'de.1'  =>  ['deutschland',        '1-bundesliga',     'Bundesliga'],
+    'de.2'  =>  ['deutschland',        '2-bundesliga2',    '2. Bundesliga'],
     'it.1'  =>  ['italy',              '1-seriea',        'Serie A'],
     'fr.1'  =>  [['europe', 'france'], '1-ligue1',        'Ligue 1'],
+
+    'at.1'  =>  ['austria',                 '1-bundesliga',   'Bundesliga'],
+    'nl.1'  =>  [['europe', 'netherlands'], '1-eredivisie',   'Eredivisie'],
+    'pt.1'  =>  [['europe', 'portugal'],    '1-primeiraliga', 'Primeira Liga'],
+
+    'uefa.cl'  =>  ['champions-league',  'cl',            'UEFA Champions League'],
 }
 
 pp datasets
 
-BASE = 'https://raw.githubusercontent.com/openfootball'
 
-seasons = %w[2024/25 2023/24]
+# seasons = %w[2020/21 2021/22 2022/23 2023/24 2024/25]
+seasons = %w[2024/25]
+
+
+root_dir = '../../openfootball'
+
+out_dir  = './tmp'
+# out_dir  = '../../openfootball/football.json'
 
 seasons.each do |season|
     season = Season( season )   ## convert to season obj
-    datasets.each do |key, ((repo,path),basename,name)|
-      url  = "#{BASE}/#{repo}"
-      url += "/master"    ## auto-add branch
-      url +=  "/#{path}"  if path
-      url +=  "/#{season.to_path}"  ## auto-add season path
-      url +=  "/#{basename}.txt"
+    datasets.each do |key, ((repo,repo_path),basename,name)|
+      path  = "#{root_dir}/#{repo}"
+      path   +=  "/#{repo_path}"  if repo_path
+      path +=  "/#{season.to_path}"  ## auto-add season path
+      path +=  "/#{basename}.txt"
 
-    puts url
+       puts path
 
-    res = Webclient.get( url )
-    sleep( 2 )
-
-    if res.status.ok?
-        matches = SportDb::QuickMatchReader.parse( res.text )
-        pp matches[0,1]
-        data = {
-            'name'  => "#{name} #{season}",
-            'matches' => matches.map {|match| match.as_json }  # convert to json
-        }
-        write_json( "./tmp/#{season.to_path}/#{key}.json", data )
-    else
-        puts "!! HTTP ERROR - #{res.status.code} #{res.status.message}"
-        exit 1
+        if File.exist?( path )
+          matches = SportDb::QuickMatchReader.read( path )
+          pp matches[0,1]
+          data = {
+              'name'  => "#{name} #{season}",
+              'matches' => matches.map {|match| match.as_json }  # convert to json
+          }
+          write_json( "#{out_dir}/#{season.to_path}/#{key}.json", data )
+        end
     end
-end
 end
 
 
